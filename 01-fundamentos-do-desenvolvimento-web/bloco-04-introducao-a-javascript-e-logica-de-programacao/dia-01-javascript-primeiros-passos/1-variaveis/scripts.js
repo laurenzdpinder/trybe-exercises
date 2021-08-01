8f12bd3956485e5788c8ca311617c21595a32d5b
Laurenz Pinder <laurenzdpinder90@gmail.com>
2021-08-01 00:58:35 -0300

    Variáveis

---
+++ 01-fundamentos-do-desenvolvimento-web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/1-variaveis/scripts.js
@@ -0,0 +1,9 @@
+const myName = "Laurenz";
+const birthCity = "Florida";
+let birthYear = 1990;
+birthYear = 2030;
+birthCity = "Rio de Janeiro";
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
