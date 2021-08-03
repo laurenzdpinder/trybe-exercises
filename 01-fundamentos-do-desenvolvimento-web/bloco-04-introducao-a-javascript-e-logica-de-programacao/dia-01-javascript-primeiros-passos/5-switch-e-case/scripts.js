b5366261620c739c0a822f991015838084aa6ee8
Laurenz Pinder <laurenzdpinder90@gmail.com>
2021-08-03 13:30:59 -0300

    Switch e Case

---
+++ 01-fundamentos-do-desenvolvimento-web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/5-switch-e-case/scripts.js
@@ -0,0 +1,18 @@
+let pessoaCanditada
+switch (pessoaCanditada) {
+  case "aprovada":
+    console.log("aprovada");
+    break;
+
+  case "lista":
+    console.log("lista");
+    break;
+
+  case "reprovada":
+    console.log("reprovada");
+    break;
+    
+  default:
+    console.log("não se aplica");
+    break;
+}
