58e37cc5ed1a7a789699effe0c63d0bfa5d6839e
Laurenz Pinder <laurenzdpinder90@gmail.com>
2021-08-30 00:33:44 -0300

    Exercícios-7.1 - Parte-II-2

--- 01-fundamentos-do-desenvolvimento-web/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/2-parte-ii/script.js
+++ 01-fundamentos-do-desenvolvimento-web/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/2-parte-ii/script.js
@@ -8,6 +8,7 @@
 
 // * Bônus (opcional): tente fazer o mesmo exercício de forma recursiva . Spoiler: É possível resolver com uma linha usando ternary operator .
 
+// Desenvolvimento
 function factorial(n) {
   if (n === 0) {
     return 1
@@ -31,6 +32,7 @@ function factorial(n) {
 }
 console.log(factorial(1));
 
+// Desenvolvimento
 
 const factorial = n => {
   let result = 1;
@@ -44,6 +46,8 @@ console.log(factorial(2));
 
 const factorial = n => n > 1 ? n * factorial(n - 1) : 1;
 console.log(factorial(5));
+
+// Raciocínio
 
 function understanding(n) {
   if (n > 0) {
@@ -59,4 +63,80 @@ console.log(understanding(4));
 4 * (3 * (2 * understanding(1)));
 4 * (3 * (2 * (1)))
 
+// Raciocínio
 
+// 2. Crie uma função que receba uma frase e retorne qual a maior palavra.
+// Exemplo:
+
+// longestWord("Antônio foi no banheiro e não sabemos o que aconteceu") // retorna 'aconteceu'
+
+let text = "Antônio foi no banheiro e não sabemos o que aconteceu";
+
+const longestWord = text => {
+  let arrayOfString = text.split(' ');
+  let maxLength = 0;
+  let result = '';
+  
+  for(const item of arrayOfString) {
+    if (item.length > maxLength) {
+      maxLength = item.length;
+      result = item;
+    }
+  }
+  return result;
+}
+
+console.log(longestWord(text));
+
+// Outras possibilidades de for
+
+// for(const item in arrayOfString) {
+//   console.log(arrayOfString[item].length)
+// }
+
+// for(let item = 0; item < arrayOfString.length; item += 1) {
+//   console.log(arrayOfString[item].length)
+// }
+
+text = "Antônio foi no banheiro e não sabemos o que aconteceu";
+
+// function stringToArray(string) {
+//   return string.split(' ');
+// }
+
+const stringToArray = string => string.split(' ');
+
+console.log(stringToArray(text));
+
+const oi = ['um', 'dois', 'dez']
+
+// function wordsLengthCompare(wordA, wordB) {
+//   return wordB.length - wordA.length;
+// }
+
+const wordsLengthCompare = (wordA, wordB) => wordB.length - wordA.length;
+
+// function sortWordsLength(array) {
+//   return array.sort(wordsLengthCompare);
+// }
+
+const sortWordsLength = array => array.sort(wordsLengthCompare);
+
+function longestWord() {
+  return sortWordsLength(oi)[0];
+}
+
+console.log(longestWord());
+
+text = "Antônio foi no banheirooooo e não sabemos o que aconteceu";
+
+// function longestWord(string) {
+//   let result = string.split(' ');
+//   result = result.sort((wordA, wordB) => wordB.length - wordA.length);
+//   result = result[0];
+//   return result;
+// }
+
+const longestWord = string => string.split(' ').sort((wordA, wordB) => wordB.length - wordA.length)[0];
+
+console.log(longestWord(text));
