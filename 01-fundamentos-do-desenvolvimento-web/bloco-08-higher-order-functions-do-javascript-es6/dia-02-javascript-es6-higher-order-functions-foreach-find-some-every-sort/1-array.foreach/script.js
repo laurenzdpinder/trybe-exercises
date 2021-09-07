c7f49f8bf2b0f8ad28fe65987349ab0e9c43427f
Laurenz Pinder <laurenzdpinder90@gmail.com>
2021-09-07 17:35:24 -0300

    Exercícios-8.2 Array.forEach

---
+++ 01-fundamentos-do-desenvolvimento-web/bloco-08-higher-order-functions-do-javascript-es6/dia-02-javascript-es6-higher-order-functions-foreach-find-some-every-sort/1-array.foreach/script.js
@@ -0,0 +1,26 @@
+// Array.forEach
+
+// Para fixar
+
+// 1 - Use o método forEach chamando a callback showEmailList para apresentar os emails
+
+const emailListInData = [
+  'roberta@email.com',
+  'paulo@email.com',
+  'anaroberta@email.com',
+  'fabiano@email.com',
+];
+
+const showEmailList = (email) => {
+  console.log(`O email ${email} esta cadastrado em nosso banco de dados!`);
+};
+
+// Adicione seu código aqui
+
+emailListInData.forEach(showEmailList);
+
+emailListInData.forEach((item) => {
+  showEmailList(item);
+})
+
+// 2 - Leia e entenda este exemplo do forEach feito no CodePen.
